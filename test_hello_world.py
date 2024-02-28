baeac3f81658329e12cb530097b243671861255a
joshua-drc <joshuadrc@gmail.com>
2024-02-28 09:17:08 -0600

    Update test

--- test_hello_world.py
+++ test_hello_world.py
@@ -5,3 +5,6 @@ from hello_world import hello_world
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello, world!")
+
+if __name__ == '__main__':
+    unittest.main()
